56bcbb5d68567be824ec0ea971d8fb3d17551ba1
Henrique Leite <henriqueleite616@gmail.com>
2022-07-13 09:06:24 -0300

    Remove Template Domain Migrations

--- src/database/migrations/migrations/index.ts
+++ src/database/migrations/migrations/index.ts
@@ -1,10 +1,6 @@
 import { migration1657471901815 } from "./1657471901815-Create-Users-Table";
 import { migration1657472036051 } from "./1657472036051-Create-VerificationTokens-Table";
 import { migration1657472138871 } from "./1657472138871-Create-RefreshTokens-Table";
-import { migration1657472436740 } from "./1657472436740-Create-Languages-Enum";
-import { migration1657472436741 } from "./1657472436741-Create-ResumeTemplates-Table";
-import { migration1657472947911 } from "./1657472947911-Create-PhrasesToFill-Table";
-import { migration1657472947912 } from "./1657472947912-Create-Months-Table";
 
 interface Migration {
 	name: string;
@@ -17,8 +13,4 @@ export const migrations: Array<Migration> = [
 	migration1657471901815,
 	migration1657472036051,
 	migration1657472138871,
-	migration1657472436740,
-	migration1657472436741,
-	migration1657472947911,
-	migration1657472947912,
 ].sort((a, b) => getCode(a.name) - getCode(b.name));
